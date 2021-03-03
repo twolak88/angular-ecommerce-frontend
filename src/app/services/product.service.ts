6c252c5464c11e269c3dd3c876ef477d48a44a8d
twolak88 <twolak88@gmail.com>
2021-03-03 22:58:13 +0100

    Implement Product Service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable,  } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { environment } from '../../environments/environment';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = environment.baseBackendServiceUrl + 'products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl)
+      .pipe(
+        map(response => response._embedded.products)
+      );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
