cb9781fcbc24bef275f51c99204383d9d6b9fcd0
twolak88 <twolak88@gmail.com>
2021-03-06 00:40:53 +0100

    Adding routing for Product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { ProductDetailsComponent } from "./components/product-list/product-details/product-details.component";
 import { ProductListComponent } from "./components/product-list/product-list.component";
 
 const appRoutes = [
   { path: 'category/:id/:name', component: ProductListComponent },
   { path: 'category', component: ProductListComponent },
   { path: 'search/:keyword', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'products', component: ProductListComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: '**', redirectTo: '/products', pathMatch: 'full' }
