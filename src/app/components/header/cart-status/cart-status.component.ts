101789239c74c0e694b1818c194e025d05e295ca
twolak88 <twolak88@gmail.com>
2021-03-07 18:01:38 +0100

    Adding refreshing Cart Status

--- src/app/components/header/cart-status/cart-status.component.ts
+++ src/app/components/header/cart-status/cart-status.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   templateUrl: './cart-status.component.html',
   styleUrls: ['./cart-status.component.css']
 })
-export class CartStatusComponent implements OnInit {
+export class CartStatusComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  subscriptions: Subscription = new Subscription();
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.updateCartStatus();
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  updateCartStatus() {
+    this.subscriptions.add(this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    ));
+    this.subscriptions.add(this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    ));
+  }
 }
