d97204115ad32636d7ce93eacd07e690fd12d921
twolak88 <twolak88@gmail.com>
2021-03-07 19:59:52 +0100

    Adding Cart Details Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SearchComponent } from './components/header/search/search.component';
 import { ProductDetailsComponent } from './components/product-list/product-details/product-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/header/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CartStatusComponent } from './components/header/cart-status/cart-status
     FooterComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit, OnDestroy {
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  subscriptions = new Subscription();
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.loadCartDetails();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private loadCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+    this.subscriptions.add(this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    ));
+    this.subscriptions.add(this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    ));
+
+    this.cartService.computeCartTotals();
+  }
+}
