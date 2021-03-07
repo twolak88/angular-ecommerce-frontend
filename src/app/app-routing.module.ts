5ec8f632c4deb7f9b1ff75151b1c0d813a649dcd
twolak88 <twolak88@gmail.com>
2021-03-07 20:00:32 +0100

    Adding Routing for Cart Details Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { CartDetailsComponent } from "./components/cart-details/cart-details.component";
 import { ProductDetailsComponent } from "./components/product-list/product-details/product-details.component";
 import { ProductListComponent } from "./components/product-list/product-list.component";
 
 const appRoutes = [
+  { path: 'cart-details', component: CartDetailsComponent },
   { path: 'category/:id/:name', component: ProductListComponent },
   { path: 'category', component: ProductListComponent },
   { path: 'search/:keyword', component: ProductListComponent },
