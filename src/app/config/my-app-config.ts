71964f3fa6edff23f78e10638e3b60bf86bb2049
twolak88 <twolak88@gmail.com>
2021-03-11 21:59:02 +0100

    Creating configuration for OpenID Connect

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oabe6l3zjJOjusJd5d6',
+    issuer: 'https://dev-72414748.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
